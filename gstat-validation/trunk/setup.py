35fb0a07e856b362e17ee640407a44455f29c4aa
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2009-10-13 10:02:36 +0000

    New version

--- gstat-validation/trunk/setup.py
+++ gstat-validation/trunk/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='gstat-validation',
-      version='2.0.15',
+      version='2.0.16',
       description='Gstat Valiation Scripts',
       long_description ='Valiation scripts for an LDAP based information system using the Glue 1.2 schema',
       author='Laurence Field',
