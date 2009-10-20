272f97f513116fb6f970f378ede03db21e76cd3d
horat <horat@4525493e-7705-40b1-a816-d608a930855b>
2009-10-20 10:50:59 +0000

    Solved ticket 50: https://svnweb.cern.ch/trac/gridinfo/ticket/50

--- gstat-web/trunk/apps/core/views.py
+++ gstat-web/trunk/apps/core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from topology.models import Entity
 import os
+
+known_types = {'grids': 'GRID',
+               'rocs': 'EGEE_ROC',
+               'tiers': 'WLCG_TIER',
+               'countries': 'Country'}
 
 def initial(request):
     file = 'firstRun'
@@ -16,3 +22,23 @@ def about(request):
 
 def register(request):
     return render_to_response('registerform.html')
+
+def filter(request, type=''):
+    options = []
+    # If no type, return all types
+    if (type == ''):
+        options = [['Select...', '-1']]
+        for key, value in known_types.iteritems():
+            options.append([value, key])
+    # If the type is known, return all values of that type
+    elif (type in known_types):
+        options = [['ALL', 'all']]
+        site_list = Entity.objects.filter(type=known_types[type]).order_by('uniqueid')
+        for site in site_list:
+            options.append([site.uniqueid, site.uniqueid])
+
+    response = render_to_response('xml', {'options': options})
+    response['Content-Type'] = 'text/xml'
+    response['Content-Disposition'] = 'attachment; filename=filter.xml'
+    response['Content-Description'] = 'XML Filter'
+    return response
