83f348b5afd3d0731cf763d57e9159980f74c4f9
horat <horat@4525493e-7705-40b1-a816-d608a930855b>
2009-10-23 14:26:23 +0000

    Solved ticket #55

--- gstat-web/trunk/apps/geo/views.py
+++ gstat-web/trunk/apps/geo/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.utils import html
 from django.shortcuts import render_to_response
-from topology.models import Entity
-from topology.models import Entityrelationship
+from topology.models import Entity, Entityrelationship
 from glue.models import gluesite
+from summary.utils import countCPUsInSite, countStoragesInSite
+import random
+from geo import countryInfo
 
 known_types = {'grids': 'GRID',
                'rocs': 'EGEE_ROC',
@@ -17,7 +19,8 @@ predicates  = {'grids': 'SiteGrid',
 
 # Stable view
 def index(request):
-    return render_to_response('geo-index.html', {'geo_active': 1})
+    #return render_to_response('geo-index.html', {'geo_active': 1})
+    return HttpResponsePermanentRedirect('/gstat/geo/openlayers')
 
 # Stable view
 def openlayers(request):
@@ -42,6 +45,39 @@ def openlayers(request):
     return render_to_response('openlayers.html',
            {'zoom':zoom, 'lon':lon, 'lat':lat, 'layers':layers, 'geo_active': 1,
             'filters_enabled': True})
+
+# Stable view
+def fullscreen(request):
+    # Restore permalinks values or use default ones
+    zoom = 2
+    if 'zoom' in request.GET:
+        zoom = request.GET['zoom'] 
+
+    lon = 5
+    if 'lon' in request.GET:
+        lon = request.GET['lon'] 
+
+    lat = 15
+    if 'lat' in request.GET:
+        lat = request.GET['lat'] 
+
+    layers = 'B000T'
+    if 'layers' in request.GET:
+        layers = request.GET['layers']
+    
+    kml = '/gstat/geo/kml'
+    if 'kml' in request.GET:
+        kml = request.GET['kml']
+
+    overlay = ''
+    if 'overlay' in request.GET:
+        overlay = request.GET['overlay']
+
+    # Render
+    return render_to_response('fullscreen.html',
+           {'zoom':zoom, 'lon':lon, 'lat':lat, 'layers':layers, 'kml':kml,
+            'overlay':overlay})
+
 
 # Stable view
 def gmap(request):
@@ -83,4 +119,49 @@ def kml(request, type='', value=''):
     response['Content-Type'] = 'application/vnd.google-earth.kml+xml'
     response['Content-Disposition'] = 'attachment; filename=gstat-map.kml'
     response['Content-Description'] = 'KML of The Grid sites'
+    return response
+
+# Development view
+def overlay(request, type=''):
+    if (type == 'egee-europe'):
+        popups = []
+        countries = Entity.objects.filter(type = 'Country')
+        egeeEntity = Entity.objects.filter(uniqueid = 'EGEE')[0]
+        for country in countries:
+            if (country.uniqueid in countryInfo.countriesInEgeeEurope):
+                site_list = []
+                for er in Entityrelationship.objects.filter(
+                                   predicate = 'SiteCountry', object = country):
+                    if (len(Entityrelationship.objects.filter(
+                                   predicate = 'SiteGrid', object = egeeEntity,
+                                   subject = er.subject)) > 0):
+                        site_list.append(er.subject)
+                if (len(site_list) > 0):
+                    logicalcpus  = 0
+                    physicalcpus = 0
+                    totalsize    = 0
+                    usedsize     = 0
+                    for site in site_list:
+                        (logicalcpus_, physicalcpus_) = countCPUsInSite(site)
+                        (totalsize_, usedsize_)       = countStoragesInSite(site)
+                        logicalcpus  += logicalcpus_
+                        physicalcpus += physicalcpus_
+                        totalsize    += totalsize_
+                        usedsize     += usedsize_
+                    totalsize = int(totalsize / 1024) # TB
+                    usedsize = int(usedsize / 1024) # TB
+                    html = "<span style='font-size:xx-small;><tt>"
+                    html += countryInfo.countriesInEgeeEurope[country.uniqueid][2]
+                    html += "<br/>s:" + str(len(site_list))
+                    html += "<br/>c:" + str(physicalcpus)
+                    html += "<br/>t:" + str(totalsize)
+                    html += "</tt></span>"
+                    popups.append([country.uniqueid,
+                                   countryInfo.countriesInEgeeEurope[country.uniqueid][0],
+                                   countryInfo.countriesInEgeeEurope[country.uniqueid][1],
+                                   html])
+
+        response = render_to_response('overlay-egee-europe.js',
+                                      {'popups': popups})
+
     return response
