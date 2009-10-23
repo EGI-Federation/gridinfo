7ff90be3c4737b0f6639bd5cd2fc3154f2c24ee3
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2009-10-23 09:10:36 +0000

    refactored summary views

--- gstat-web/trunk/apps/summary/urls.py
+++ gstat-web/trunk/apps/summary/urls.py
@@ -1,18 +1,11 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('summary.views',
-    (r'^$', 'summary'),
-    (r'(?P<type>top)/$', 'bdii_view'),
-    (r'(?P<type>site)/$', 'bdii_view'),
-    (r'^grid/$', 'grid'),
-    (r'^grid/EGEE/$', 'egee'),
-    (r'^grid/egee_roc/$', 'egee_roc'),
-    (r'^grid/egee_roc/(?P<roc_name>.+)/$', 'egee_roc_specified'),
-    (r'^grid/egee_service/(?P<service_name>.+)/$', 'egee_service_specified'),
-    (r'^grid/WLCG/$', 'wlcg_tier'),
-    (r'^grid/wlcg_tier/(?P<tier_name>.+)/$', 'wlcg_tier_specified'),
-    (r'^grid/(?P<grid_name>.+)/$', 'grid_specified'),
-    (r'^country/$', 'country'),
-    (r'^country/(?P<country_name>.+)/$', 'country_specified'),
+    (r'^$', 'main'),
+    url(r'^json$', 'main', kwargs={'output': 'json'}),
+    (r'^(?P<type>\w+)/$', 'main'),
+    url(r'^(?P<type>\w+)/json$', 'main',kwargs={'output': 'json'} ),
+    (r'^(?P<type>\w+)/(?P<value>\w+)/$', 'main'),
+    url(r'^(?P<type>\w+)/(?P<value>\w+)/json', 'main', kwargs={'output': 'json'}),
 )
 
