3a5b0a47f08e3cc54d5f07f838a6a4f3e7baa341
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2009-10-28 15:00:24 +0000

    update

--- gstat-web/trunk/apps/gridsite/urls.py
+++ gstat-web/trunk/apps/gridsite/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gridsite.views',
-    (r'^(?P<site_name>.+)/topbdii/$', 'topbdii'),
-    (r'^(?P<site_name>.+)/topbdii_xml/$', 'topbdii_xml'),
-    (r'^(?P<site_name>.+)/topbdii_json/$', 'topbdii_json'),
-    (r'^(?P<site_name>.+)/sitebdii/$', 'sitebdii'),
-    (r'^(?P<site_name>.+)/ce/$', 'ce'),
-    (r'^(?P<site_name>.+)/se/$', 'se'),
-    (r'^(?P<site_name>.+)/service/$', 'service'),
-    (r'^(?P<site_name>.+)/site/$', 'site'),
+    (r'^(?P<site_name>.+)/graphs/(?P<attribute>.+)/(?P<vo_name>[^/]*)/$', 'vo_graphs'),
+    (r'^(?P<site_name>.+)/graphs/(?P<attribute>.+)/$', 'site_graphs'),
+    (r'^(?P<site_name>.+)/(?P<type>.+)/$', 'status'),
     (r'^(?P<site_name>.+)/$', 'overview'),
 )
