6545181d37ada6a3a28f2f6badb61621394b9dd5
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2009-10-30 22:14:24 +0000

    update

--- gstat-web/trunk/apps/summary/urls.py
+++ gstat-web/trunk/apps/summary/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('summary.views',
-    (r'^$', 'main'),
-    url(r'^json$', 'main', kwargs={'output': 'json'}),
+    (r'^(?P<type>\w+)/(?P<value>[A-Za-z0-9- :/._]+)/json/$', 'main', {'output': 'json'}),
+    (r'^(?P<type>\w+)/json/$', 'main', {'output': 'json'}),
+    (r'^(?P<type>\w+)/(?P<value>[A-Za-z0-9- :/._]+)/$', 'main'),
+    (r'^json/$', 'main', {'output': 'json'}),
     (r'^(?P<type>\w+)/$', 'main'),
-    url(r'^(?P<type>\w+)/json$', 'main',kwargs={'output': 'json'} ),
-    (r'^(?P<type>\w+)/(?P<value>\w+)/$', 'main'),
-    url(r'^(?P<type>\w+)/(?P<value>\w+)/json', 'main', kwargs={'output': 'json'}),
+    (r'^$', 'main'),
 )
 
