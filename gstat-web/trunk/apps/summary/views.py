0c928f53bef29a9aba1ba7bd9f5b9dc07ebdbc55
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2009-10-30 22:14:24 +0000

    update

--- gstat-web/trunk/apps/summary/views.py
+++ gstat-web/trunk/apps/summary/views.py
@@ -10,61 +10,72 @@ from topology.models import Entityrelationship
 from core.utils import *
 
 def main(request, type=None, value=None, output=None):
-    predicate = {'Grid': 'SiteGrid', 
-               'EGEE_ROC': 'SiteEgeeRoc', 
-               'WLCG_TIER': 'SiteWlcgTier',
-               'County': 'SiteCountry'
-               }
+    predicate = {'GRID'     : 'SiteGrid', 
+                 'EGEE_ROC' : 'SiteEgeeRoc', 
+                 'WLCG_TIER': 'SiteWlcgTier',
+                 'Country'   : 'SiteCountry'}
 
     data = []
-    if ( type == None ):
-        type = 'Grid'
+    if (type == None):
+        type = 'GRID'
     
-    if ( value == "ALL" ):
-        entities = Entity.objects.filter(type=type)
-        for entity in entities:
-            site_list = getSitesInGroup(predicate[type], entity)
-            group  = get_data_for_sites(site_list)
-            summary = [str(entity.uniqueid), 0, 0, 0, 0, 0, 0, 0]
-            for row in group:
+    if (value == "ALL" or value == None):
+        # group list, ex: list of grid, list of roc, list of country
+        groups = getEntitiesByType(type)
+        for group in groups:
+            site_list = getSitesInGroup(predicate[type], group)
+            sites_data_rows  = get_data_for_sites(site_list)
+            group_summary = [str(group.uniqueid), 0, 0, 0, 0, 0, 0, 0, 0]
+            for row in sites_data_rows:
+                group_summary[1] += 1
                 for i in range(7):
-                    summary[i + 1] += row[i + 1]
-            data.append(summary)       
+                    group_summary[i + 2] += row[i + 2]
+            data.append(group_summary)       
     else:
-        entity = Entity.objects.get(uniqueid__iexact=value, type=type)
-        site_list = getSitesInGroup(predicate[type], entity)
-        data = get_data_for_sites(site_list)
+        # site list
+        group = getEntityByUniqueidType(value, type)
+        site_list = getSitesInGroup(predicate[type], group)
+        data = get_data_for_sites(site_list, get_status=True)
 
     if (output == 'json'):
-        content = '{ "aaData": %s }' % (json.dumps(data))
+        content = '{"aaData": %s}' % (json.dumps(data))
         return HttpResponse(content, mimetype='application/json')  
     else:
         breadcrumbs_list = [{'name':'Summary', 'url':'/gstat/summary/'}]
-        thead = ["Name", "Logical CPUs", "Physical CPUs", "Total Space", "Used Space", "Running Jobs", "Waiting Jobs", "Total Jobs"]
 
-        return render_to_response('single_table.html', {'summary_active': 1,
-                                                    'breadcrumbs_list': breadcrumbs_list,
-                                                        'filters_enabled': True,
-                                                        'thead': thead})
-def get_data_for_sites(site_list):
-    data = [ ]
+        return render_to_response('single_table.html', {'summary_active'  : 1,
+                                                        'breadcrumbs_list': breadcrumbs_list,
+                                                        'filters_enabled' : True})
+def get_data_for_sites(site_list, get_status=False):
+    data = []
+    nagios_status = {}
     for site in site_list:
         # Get installed capacities for site 
-        (logical_cpus, physical_cpus)           = countCPUsInSite(site)
-        (totalonlinesize, usedonlinesize, totalnearlinesize, usednearlinesize) = countStoragesInSite(site)
-        (running_jobs, waiting_jobs, total_jobs) = countJobsInSite(site)
         site_name = str(site.uniqueid)
+        (logical_cpus, physical_cpus) = countCPUsInSite(site)
+        (totalonlinesize, usedonlinesize, totalnearlinesize, usednearlinesize) = countStoragesInSite(site)
         total_size = totalonlinesize + totalnearlinesize
         used_size  = usedonlinesize + usednearlinesize
+        (total_jobs, running_jobs, waiting_jobs) = countJobsInSite(site)
+        site_number_or_status = 0
+        if get_status:
+            if not nagios_status: nagios_status = getNagiosStatusDict()
+            topbdii_list  = getNodesInSite(site, 'bdii_top')
+            sitebdii_list = getNodesInSite(site, 'bdii_site')
+            hostnames = [bdii.hostname for bdii in topbdii_list+sitebdii_list]
+            (status, has_been_checked) = getNodesOverallStatus(nagios_status, hostnames, '^check-.+')
+            site_number_or_status = getNagiosStatusStr(status, has_been_checked)
+
         
-        row = [ site_name, 
-                logical_cpus, 
-                physical_cpus, 
+        row = [ site_name,
+                site_number_or_status,
+                physical_cpus,
+                logical_cpus,  
                 total_size, 
                 used_size, 
+                total_jobs,
                 running_jobs, 
-                waiting_jobs, 
-                total_jobs ]
+                waiting_jobs]
         data.append(row)
 
     return data
