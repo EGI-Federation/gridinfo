d7a5eec829625e36090db1feec59a1868cd9316f
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2009-11-04 10:54:08 +0000

    New Release

--- gstat-web/tags/R_2_0_18/apps/gridsite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('gridsite.views',
-    (r'^(?P<site_name>.+)/graphs/(?P<attribute>.+)/(?P<vo_name>[^/]*)/$', 'vo_graphs'),
-    (r'^(?P<site_name>.+)/graphs/(?P<attribute>.+)/$', 'site_graphs'),
-    (r'^(?P<site_name>.+)/(?P<type>.+)/$', 'status'),
-    (r'^(?P<site_name>.+)/$', 'overview'),
-)
