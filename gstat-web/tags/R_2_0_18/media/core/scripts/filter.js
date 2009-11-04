c7975762dcafc949e05bf266c659dddf34765867
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2009-11-04 10:47:17 +0000

    New Release

---
+++ gstat-web/tags/R_2_0_18/media/core/scripts/filter.js
@@ -0,0 +1,59 @@
+function loadJSONDoc(url) {
+	$.getJSON(url,
+	  function(data){
+	    clearList('filtervalue');
+	    var select = document.getElementById('filtervalue');
+	    appendToSelect(select, '-1', document.createTextNode('Select a value'));
+        $.each(data.options, function(i,option){
+          if (option != null) {
+            appendToSelect(select, option.value, document.createTextNode(option.key));
+          }
+        });
+      }
+    );
+}
+
+// Invoked by "filtertype" select element change
+// Clears "filtervalue" select and loads new values
+function changeFilterType(evt) {
+    // equalize W3C/IE event models to get event object
+    evt = (evt) ? evt : ((window.event) ? window.event : null);
+    if (evt) {
+        // equalize W3C/IE models to get event target reference
+        var elem = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+        if (elem[elem.selectedIndex].value == 'none') {
+        	changeFilterValue(evt);
+        } else {
+	        if (elem) {
+	            try {
+	                if (elem.selectedIndex > 0) {
+	                    loadJSONDoc("/gstat/core/filter/" + elem.options[elem.selectedIndex].value);
+	                }	
+	            }
+	            catch(e) {
+	                var msg = (typeof e == "string") ? e : ((e.message) ? e.message : "Unknown Error");
+	                alert("Unable to get JSON data:\n" + msg);
+	                return;
+	            }
+	        }
+	    }
+    }
+}
+
+// empties a select list content
+function clearList(listId) {
+    var select = document.getElementById(listId);
+    while (select.length > 0) {
+        select.remove(0);
+    }
+}
+
+// add item to select element the less
+// elegant, but compatible way.
+function appendToSelect(select, value, content) {
+    var opt;
+    opt = document.createElement("option");
+    opt.value = value;
+    opt.appendChild(content);
+    select.appendChild(opt);
+}
