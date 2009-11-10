53355432af86596008a746d86b2cda1f83418b67
horat <horat@4525493e-7705-40b1-a816-d608a930855b>
2009-11-10 13:49:49 +0000

    Improved URL tests

--- gstat-web/trunk/apps/core/tests.py
+++ gstat-web/trunk/apps/core/tests.py
@@ -1,13 +1,33 @@
 from django.test import TestCase
 from django.test.client import Client
 
-class GeoTestCase(TestCase):
+class CoreTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
     def testIndex(self):
         response = self.client.get('/')
         self.assertRedirects(response,
-                             '/gstat/geo/openlayers',
-                             status_code=302,
-                             target_status_code=200)
+                           '/gstat/geo/openlayers',
+                           status_code = 302,
+                           target_status_code = 200)
+
+    def testFilter(self):
+        response = self.client.get('/gstat/core/filter')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def testFilter(self):
+        response = self.client.get('/gstat/core/filter/GRID')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def testFilter(self):
+        response = self.client.get('/gstat/core/filter/EGEE_ROC')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def testFilter(self):
+        response = self.client.get('/gstat/core/filter/WLCG_TIER')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def testFilter(self):
+        response = self.client.get('/gstat/core/filter/Country')
+        self.failUnlessEqual(response.status_code, 200)
