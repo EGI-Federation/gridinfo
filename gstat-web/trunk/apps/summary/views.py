8f263194ed3ba6ad261037e6b72087502c88871f
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2010-02-05 10:49:55 +0000

    Performance Improvements

--- gstat-web/trunk/apps/summary/views.py
+++ gstat-web/trunk/apps/summary/views.py
@@ -1,100 +1,63 @@
 from django.shortcuts import get_object_or_404, render_to_response
+from django.views.decorators.cache import cache_page
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils import html
 import gsutils
-import socket
 from django.utils import simplejson as json
 from topology.models import Entity
 from topology.models import Entityrelationship
 from core.utils import *
+import time
 
-def main(request, type='GRID', value='ALL', output = None):
+def main(request, type='GRID', value='ALL'):
 
-    if ( output == None ):
-         breadcrumbs_list = [{'name':'Summary', 'url':'/gstat/summary/'}]
-         return render_to_response('single_table.html', 
-                                   {'summary_active'  : 1,
-                                    'breadcrumbs_list': breadcrumbs_list,
-                                    'type'            : type,
-                                    'value'           : value,
-                                    'filters_enabled' : True})
+    breadcrumbs_list = [{'name':'Summary', 'url':'/gstat/summary/'}]
+    return render_to_response('single_table.html', 
+                              {'summary_active'  : 1,
+                               'breadcrumbs_list': breadcrumbs_list,
+                               'type'            : type,
+                               'value'           : value,
+                               'filters_enabled' : True})
 
-
+@cache_page(60 * 10)
+def get_json(request, type='GRID', value='ALL'):
     data = []
-    
     if (value == "ALL"):
-        # group list, ex: list of grid, list of roc, list of country
         groups = get_groups(type=type)
+        topology = {}
+        site_list = []
         for group in groups:
-            site_list = get_sites(type=type, value=group.uniqueid, groups=[group])
-            sites_data_rows  = get_data_for_sites(site_list)
-            group_summary = [str(group.uniqueid), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            for row in sites_data_rows:
+            topology[group.uniqueid]=[]
+            sites = get_sites(type=type, value=group.uniqueid, groups=[group])
+            site_list.extend(sites)       
+            for site in sites:
+                topology[group.uniqueid].append(site.uniqueid)
+    else:
+        site_list = get_sites(type, value)
+    if (value == "ALL"):
+        site_data  = get_installed_capacities(site_list)
+
+        for group in topology.keys():
+            group_summary = [group, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            for site_id in topology[group]:
                 group_summary[1] += 1
                 for i in range(9):
-                    group_summary[i + 2] += row[i + 2]
-            data.append(group_summary)       
+                    group_summary[i + 2] += site_data[site_id][i]
+            data.append(group_summary)  
     else:
-        # site list
-        site_list = get_sites(type, value)
-        if type == 'VO':
-            data = get_data_for_sites(site_list, get_status=True, vo_name=value)
-        else:   
-            data = get_data_for_sites(site_list, get_status=True)
-
-    if (output == 'json'):
-        content = '{"aaData": %s}' % (json.dumps(data))
-        return HttpResponse(content, mimetype='application/json')  
-        
-def get_data_for_sites(site_list, get_status=False, vo_name=False):
-    data = []
-    nagios_status = {}
-    for site in site_list:
-        # Get installed capacities for site 
-        site_name = str(site.uniqueid)
-        
-        service_list = get_services([site])
-        sub_cluster_list = get_gluesubclusters(service_list)
-        physical_cpu, logical_cpu = get_installed_capacity_cpu(sub_cluster_list)
-        
-        if vo_name:
-            sa_list = get_gluesas(service_list, vo_name)
-            total_online, used_online, total_nearline, used_nearline = get_sa_storage_stats(sa_list)
-            
-            vo_view_list = get_gluevoviews(service_list, vo_name)
-            total_jobs, running_jobs, waiting_jobs = get_voview_job_stats(vo_view_list)
+        site_data = None
+        if ( type == 'VO'):
+            site_data = get_installed_capacities(site_list, vo_name=value)
         else:
-            se_list = get_glueses(service_list)
-            total_online, used_online, total_nearline, used_nearline = get_installed_capacity_storage(se_list)
-            
-            vo_view_list = get_gluevoviews(service_list)
-            total_jobs, running_jobs, waiting_jobs = get_voview_job_stats(vo_view_list)        
-        
-        site_number_or_status = 0
-        if get_status:
-            if not nagios_status: nagios_status = get_nagios_status_dict()
-            topbdii_list  = []
-            sitebdii_list = []
-            for service in service_list:
-                if service.type == 'bdii_top':  topbdii_list.append(service)
-                if service.type == 'bdii_site': sitebdii_list.append(service)       
-            hostnames = [bdii.hostname for bdii in topbdii_list+sitebdii_list]
-            (status, has_been_checked) = get_hosts_overall_nagios_status(nagios_status, hostnames, '^check-.+')
-            site_number_or_status = get_nagios_status_str(status, has_been_checked)
-            
-        row = [ site_name,
-                site_number_or_status,
-                physical_cpu,
-                logical_cpu,  
-                total_online, 
-                used_online, 
-                total_nearline,
-                used_nearline,
-                total_jobs,
-                running_jobs, 
-                waiting_jobs]
-        data.append(row)
+            site_data = get_installed_capacities(site_list)
+        start=time.time()
+        status_data = get_status_for_sites(site_list)
+        print "T2 = %s\n" %(time.time()-start)
+        for site_id in site_data.keys():
+            row = [site_id, status_data[site_id]]
+            row.extend(site_data[site_id])
+            data.append(row)
 
-    return data
-
+    content = '{"aaData": %s}' % (json.dumps(data))
+    return HttpResponse(content, mimetype='application/json')  
