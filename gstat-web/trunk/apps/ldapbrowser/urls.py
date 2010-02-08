0ae64f1f8537b573f349d220f59480251e6341ed
horat <horat@4525493e-7705-40b1-a816-d608a930855b>
2010-02-08 11:58:51 +0000

    Solved ticket #136

--- gstat-web/trunk/apps/ldapbrowser/urls.py
+++ gstat-web/trunk/apps/ldapbrowser/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('ldapbrowser.views',
     (r'^$', 'index'),
     (r'^browse', 'browse'),
     (r'^site/(?P<url>[A-Za-z0-9- :/."_=]+)$', 'index'),
+    (r'^server/(?P<ldapurl>[A-Za-z0-9- :/."_=,]+)$', 'index'),
 )
