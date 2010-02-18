543c3d45e806d21e3801d5fc8aa11ae84839bfb9
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2010-02-18 14:00:54 +0000

    New Release

---
+++ gstat-web/tags/R_0_0_22/media/core/scripts/utils.js
@@ -0,0 +1,38 @@
+
+function CommaFormatted(amount)
+{   
+    var isInt=parseInt(amount);
+    if (isNaN(isInt)) return amount; 
+    if (amount == 0) return amount;
+    amount=amount.toString().replace(/^0+/, ''); 
+	amount += '';
+	x = amount.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
+//To remove the comma for the variables
+function RemoveCommaFormatted(amount) {
+  amount= amount.replace(/,/g,"");
+  return amount;
+}
+
+function TimeFormat(ms) {
+    var m_names = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+    if (ms != undefined) {
+	    var d = new Date(parseInt(ms) * 1000);
+	    var curr_date = d.getUTCDate();
+	    var curr_month = d.getUTCMonth();
+	    //curr_month++;
+	    var curr_year = d.getUTCFullYear();
+	    var curr_hour = d.getUTCHours();
+	    var curr_min = d.getUTCMinutes();
+	    var curr_sec = d.getUTCSeconds();
+	    document.write(curr_date+"-"+m_names[curr_month]+"-"+curr_year+" "+curr_hour+":"+curr_min+":"+curr_sec+" UTC");
+    }
+}
