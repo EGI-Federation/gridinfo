323c4308e4541890f62165ed9ecf80fd7c1d94e6
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2010-02-18 14:09:42 +0000

    Mistake

--- gstat-web/tags/R_0_0_22/trunk/apps/gridsite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('gridsite.views',
-    (r'^(?P<site_name>.+)/treeview/(?P<type>.+)/(?P<attribute>.+)/$', 'treeview'),
-    (r'^(?P<site_name>.+)/treeview/(?P<type>.+)/$', 'treeview'),
-    (r'^(?P<site_name>.+)/(?P<type_name>.+)/(?P<host_name>.+)/(?P<check_name>.+)/$', 'status'),
-    (r'^(?P<site_name>.+)/$', 'overview'),
-)
--- gstat-web/tags/R_0_0_22/trunk/apps/vo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('vo.views',
-    (r'^(?P<vo_name>.+)/overview/$', 'overview'),
-    (r'^(?P<vo_name>.+)/$', 'treeview'),
-    (r'^$', 'treeview'),
-)
