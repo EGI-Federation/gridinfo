fd883bf372c3474bd923475e43fe1e8e79b6c4ae
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2010-03-30 04:32:12 +0000

    fixed ticket #211

--- gstat-web/trunk/apps/vo/views.py
+++ gstat-web/trunk/apps/vo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404
 from django.core.urlresolvers import reverse
 from django.utils import html
 from core.utils import *
+from topology.models import Entity
 import gsutils
 import socket
 import re
@@ -10,6 +11,9 @@ import time
 
 
 def treeview(request, vo_name=""):
+    # get vo list
+    vo_list = Entity.objects.filter(type="VO").order_by('uniqueid')
+    
     #vo_list = get_groups('VO')
     vo_site_ce = get_vo_site_service(vo_name, 'CE')
     vo_site_se = get_vo_site_service(vo_name, 'SE')
@@ -38,36 +42,12 @@ def treeview(request, vo_name=""):
         
     # decide the default viewing content in iframe of treeview page
     url = ""
-    if type == "bdii_top":
-        # e.g. /gstat/site/CERN-PROD/bdii_top/bdii118.cern.ch/all/
-        url = "/".join(["", "gstat", "site", site_name, type, attribute, "all"])
-    elif type == "bdii_site":
-        # e.g. /gstat/site/CERN-PROD/bdii_site/bdii116.cern.ch/all/
-        url = "/".join(["", "gstat", "site", site_name, type, attribute, "all"])
-    elif type == "subcluster_cpu":
-        # e.g. /gstat/rrd/Site/CERN-PROD/cpu/
-        url = "/".join(["", "gstat", "rrd", "Site", site_name, "cpu"])
-    elif type == "se_online":
-        # e.g. /gstat/rrd/Site/CERN-PROD/online/
-        url = "/".join(["", "gstat", "rrd", "Site", site_name, "online"])
-    elif type == "se_nearline":
-        # e.g. /gstat/rrd/Site/CERN-PROD/nearline/
-        url = "/".join(["", "gstat", "rrd", "Site", site_name, "nearline"])
-    elif type == "vo_job":
-        # e.g. /gstat/rrd/VOSite/CERN-PROD/alice/job/
-        url = "/".join(["", "gstat", "rrd", "VOSite", site_name, attribute, "job"])
-    elif type == "vo_online":
-        # e.g. /gstat/rrd/VOSite/CERN-PROD/alice/online/
-        url = "/".join(["", "gstat", "rrd", "VOSite", site_name, attribute, "online"])
-    elif type == "vo_nearline":
-        # e.g. /gstat/rrd/VOSite/CERN-PROD/alice/nearline/
-        url = "/".join(["", "gstat", "rrd", "VOSite", site_name, attribute, "nearline"])
 
     return render_to_response('treeview_vo.html', {'vo_active':        1,
+                                                   'vo_list':          vo_list,
                                                    'vo_name':          vo_name,
                                                    'collapse':         collapse,
                                                    'tree_vo':          tree_vo,
-                                                   'url':              url,
-                                                   'filters_enabled':  True})
+                                                   'url':              url})
 
 
