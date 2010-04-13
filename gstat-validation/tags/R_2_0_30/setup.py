63f93c6394d77d9cf460d148e62eb297c66ef282
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2010-04-13 11:10:42 +0000

    New Release

---
+++ gstat-validation/tags/R_2_0_30/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gstat-validation',
+      version='2.0.30',
+      description='Gstat Valiation Scripts',
+      long_description ='Valiation scripts for an LDAP based information system using the Glue 1.2 schema',
+      author='Laurence Field',
+      author_email='Laurence.Field@cern.ch',
+      license='EGEE',
+      url='http://goc.grid.sinica.edu.tw/gocwiki/GSIndex',
+      scripts = ['bin/gstat-validate-ce', 'bin/gstat-validate-sanity-check', 'bin/gstat-validate-site', 'bin/gstat-validate-se', 'bin/gstat-validate-service'],
+      py_modules=['gsutils'],
+      package_dir = {'': 'src'},
+      data_files = [
+        ('/usr/share/gstat/', ['data/locations', 'data/wlcg-tier', 'data/service-types']),
+        ],
+      )
