67124661f7fa1470cb89f7677ca15bbd809c7404
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2010-04-14 09:14:11 +0000

    fixed the bug on Service View

--- gstat-web/trunk/media/core/scripts/utils.js
+++ gstat-web/trunk/media/core/scripts/utils.js
@@ -23,16 +23,21 @@ function RemoveCommaFormatted(amount) {
 }
 
 function TimeFormat(ms) {
-    var m_names = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-    if (ms != undefined) {
-	    var d = new Date(parseInt(ms) * 1000);
-	    var curr_date = d.getUTCDate();
-	    var curr_month = d.getUTCMonth();
-	    //curr_month++;
-	    var curr_year = d.getUTCFullYear();
-	    var curr_hour = d.getUTCHours();
-	    var curr_min = d.getUTCMinutes();
-	    var curr_sec = d.getUTCSeconds();
-	    document.write(curr_date+"-"+m_names[curr_month]+"-"+curr_year+" "+curr_hour+":"+curr_min+":"+curr_sec+" UTC");
+    var isInt=parseInt(ms);
+    if (isNaN(isInt)) {
+        document.write("N/A"); 
+    } else {
+	    var m_names = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+	    if (ms != undefined) {
+		    var d = new Date(parseInt(ms) * 1000);
+		    var curr_date = d.getUTCDate();
+		    var curr_month = d.getUTCMonth();
+		    //curr_month++;
+		    var curr_year = d.getUTCFullYear();
+		    var curr_hour = d.getUTCHours();
+		    var curr_min = d.getUTCMinutes();
+		    var curr_sec = d.getUTCSeconds();
+		    document.write(curr_date+"-"+m_names[curr_month]+"-"+curr_year+" "+curr_hour+":"+curr_min+":"+curr_sec+" UTC");
+	    }
     }
 }
