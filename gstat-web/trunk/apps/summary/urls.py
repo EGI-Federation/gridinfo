700c778681316c74aaf8aded700452270b5a8f7d
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2010-11-17 03:05:25 +0000

    IS-167: public JSON feed of installed capacity

--- gstat-web/trunk/apps/summary/urls.py
+++ gstat-web/trunk/apps/summary/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('summary.views',
     (r'^(?P<type>\w+)/(?P<value>[A-Za-z0-9- :/."_=]+)/json/$', 'get_json'),
     (r'^(?P<type>\w+)/json/$', 'get_json'),
     (r'^(?P<type>\w+)/(?P<value>[A-Za-z0-9- :/."_=]+)/$', 'main'),
-    (r'^json/$', 'get_json'),
+    (r'^json/$', 'get_public_json'),
     (r'^(?P<type>\w+)/$', 'main'),
     (r'^$', 'main'),
 )
