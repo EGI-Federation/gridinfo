2a6cda7cbd45c91bec7d1b01bb32af875739147e
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2011-02-10 13:57:16 +0000

    Aded Interface Name types

--- glue-validator/trunk/lib/glue2/types.py
+++ glue-validator/trunk/lib/glue2/types.py
@@ -88,6 +88,7 @@ def is_UInt64( value):
 def is_DateTime_t( value):
    # Check http://www.w3.org/TR/xmlschema-2/#dateTime
    dateTime = "^-?[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])T([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]Z?$"
+2010-12-02T17:22:37+00:00
    if re.match(dateTime, value):
       return True
    return False
@@ -106,11 +107,7 @@ def is_QualityLevel_t(value):
       return False
 
 def is_InterfaceName_t(value):
-   interfaces = [ 'ogf.bes', 'ogf.srm' ]
-   if value in interfaces:
-      return True
-   else:
-      return False
+   return is_ServiceType_t(value)
 
 def is_PolicyScheme_t(value):
    schemes = ['basic', 'gacl', 'org.glite.standard']
