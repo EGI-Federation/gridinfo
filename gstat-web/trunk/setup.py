127e30f10b61b3e4eaa723c88e5e598c9e5f8519
hhuang <hhuang@4525493e-7705-40b1-a816-d608a930855b>
2011-02-14 06:50:28 +0000

    New Version

--- gstat-web/trunk/setup.py
+++ gstat-web/trunk/setup.py
@@ -20,7 +20,7 @@ def get_files(directory, install_base):
 media_files = get_files("media", "share/gstat")
 
 setup(name='gstat-web',
-      version='1.0.11',
+      version='1.0.12',
       py_modules=['gsutils'],
       package_dir = {'': 'apps'},
       scripts =['tools/configure-nagios',
