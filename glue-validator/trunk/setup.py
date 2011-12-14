fca9a87a122ea1edb1497cecbf441a53f92d6c2b
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2011-12-14 12:26:41 +0000

    More fixes for Steve

--- glue-validator/trunk/setup.py
+++ glue-validator/trunk/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='glue-validator',
-      version='1.0.0',
+      version='1.0.2',
       description='GLUE 2.0 Valiation',
       long_description ='Valiation scripts for an LDAP based information system using the GLUE 2.0 schema',
       author='Laurence Field',
