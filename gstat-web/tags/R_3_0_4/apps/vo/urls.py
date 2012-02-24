e59e5b0adee386d1903dcfe3cd58fa9071b3e348
lfield <lfield@4525493e-7705-40b1-a816-d608a930855b>
2012-02-24 09:15:25 +0000

    Deleted bad tag

--- gstat-web/tags/R_3_0_4/apps/vo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('vo.views',
-    (r'^(?P<vo_name>.+)/(?P<type>\w+)/json/$', 'get_json'),
-    (r'^(?P<vo_name>.+)/overview/$', 'overview'),
-    (r'^(?P<vo_name>.+)/$', 'treeview'),
-    (r'^$', 'treeview'),
-)
