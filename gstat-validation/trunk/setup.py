85f3b9c862cc9b68d390016d4b9e2818d8e68cbd
jyou <jyou@4525493e-7705-40b1-a816-d608a930855b>
2012-02-28 16:32:20 +0000

    Fix for GGUS #78364

--- gstat-validation/trunk/setup.py
+++ gstat-validation/trunk/setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='gstat-validation',
-      version='2.0.52',
+      version='2.0.53',
       description='Gstat Valiation Scripts',
       long_description ='Valiation scripts for an LDAP based information system using the Glue 1.2 schema',
-      author='Laurence Field',
-      author_email='Laurence.Field@cern.ch',
+      author='Jingya You',
+      author_email='jingya.you@cern.ch',
       license='EGEE',
       url='http://goc.grid.sinica.edu.tw/gocwiki/GSIndex',
       scripts = ['bin/gstat-validate-ce', 'bin/gstat-validate-sanity-check', 'bin/gstat-validate-site', 'bin/gstat-validate-se', 'bin/gstat-validate-service'],
