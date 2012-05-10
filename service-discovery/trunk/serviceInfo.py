529ee1a40c6ba03e208b8bc0183142c786e3d678
icalvet <icalvet@4525493e-7705-40b1-a816-d608a930855b>
2012-05-10 15:39:04 +0000

    Change of name

--- service-discovery/trunk/serviceInfo.py
+++ service-discovery/trunk/serviceInfo.py
@@ -76,16 +76,16 @@ def main(argv):
 	sys.exit()
 
 def usage():
-	print '''Usage: service-info [options] 
+	print '''Usage: service-info [options]
 
-    -b, --bdii     host:port	Specify a BDII endpoint (<hostname>:<port>). By default the environmental variable LCG_GFAL_INFOSYS will be used.
-    -t, --type    type		Lists all services of a specific type. By default all services are returned.
-    -v, --vo      VO   		List all services for a specific VO
-    -l, --list	  attrib 	List all the published values for an attribute (ServiceType, VO)
+    -b, --bdii	host:port	Specify a BDII endpoint (<hostname>:<port>). By default the environmental variable LCG_GFAL_INFOSYS will be used.
+    -t, --type	type		Lists all services of a specific type. By default all services are returned.
+    -v, --vo	VO		List all services for a specific VO
+    -l, --list	attrib		List all the published values for an attribute (ServiceType, VO)
     -c, --csv			Provides the output in CSV formating
     -j, --json			Provides the output in JSON formating
-    -h, --help 	   		Prints this helpful message
-    -d, --debug    		Debug level: 0(errors), 1(warnings), 2(info), 3(debug), default 2
+    -h, --help			Prints this helpful message
+    -d, --debug			Debug level: 0(errors), 1(warnings), 2(info), 3(debug), default 2
     -V, --version		Prints this helpful message'''
 
 def discover(bdii,type='',output=0,debug='2'):
@@ -98,19 +98,27 @@ def discover(bdii,type='',output=0,debug='2'):
 				continue
 		list.append(r[0][r[0].find('=')+1:r[0].find(',')])
 	list.sort()
-	for i in list:
-		print i
+	if output == 0:
+		print '\n'.join(list)
+	elif output == 1:
+		print ','.join(list)
+	elif output == 2:
+		list = '\':[],\''.join(list)
+		print '{\''+list+'\':[]}'
 
 def list_attr(bdii,attr=''):
-	l = ldap.initialize('ldap://'+bdii)
-        res = l.result(l.search('o=glue',ldap.SCOPE_SUBTREE,'(objectClass=GLUE2Service)'))
-        list=[]
-	for r in res[1]:
-		if r[1]['GLUE2'+attr][0] not in list:
-			list.append(r[1]['GLUE2ServiceType'][0])
-	list.sort()
-        for i in list:
-                print i
+	if attr == 'ServiceType':
+		l = ldap.initialize('ldap://'+bdii)
+        	res = l.result(l.search('o=glue',ldap.SCOPE_SUBTREE,'(objectClass=GLUE2Service)'))
+        	list=[]
+		for r in res[1]:
+			if r[1]['GLUE2'+attr][0] not in list:
+				list.append(r[1]['GLUE2ServiceType'][0])
+		list.sort()
+        	for i in list:
+        	        print i
+	else:
+		print 'Error: wrong attribute.'
 
 if __name__ == "__main__":
     main(sys.argv[1:])
