586ac8f1d13edf593ba087bb982a6f223119fd09
jyou <jyou@4525493e-7705-40b1-a816-d608a930855b>
2013-08-19 07:35:58 +0000

    Fix for supporting ARC-CE as one of the service-types in gstat-validation-services

---
+++ gstat-validation/tags/R_2_0_54/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gstat-validation',
+      version='2.0.54',
+      description='Gstat Valiation Scripts',
+      long_description ='Valiation scripts for an LDAP based information system using the Glue 1.2 schema',
+      author='Jingya You',
+      author_email='jingya.you@cern.ch',
+      license='EGEE',
+      url='http://goc.grid.sinica.edu.tw/gocwiki/GSIndex',
+      scripts = ['bin/gstat-validate-ce', 'bin/gstat-validate-sanity-check', 'bin/gstat-validate-site', 'bin/gstat-validate-se', 'bin/gstat-validate-service'],
+      py_modules=['gsutils'],
+      package_dir = {'': 'src'},
+      data_files = [
+        ('/var/lib/gstat/', ['data/locations', 'data/wlcg-tier', 'data/service-types', 'data/NGI.xml']),
+        ],
+      )
