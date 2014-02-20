8e5ed0b5dba76e1f9e94a5d8e79e9dca137cbb32
malandes <malandes@4525493e-7705-40b1-a816-d608a930855b>
2014-02-20 14:40:11 +0000

    Add waiting_jobs specific action

--- monitoring-scripts/trunk/monitoring/wlcg-glue2-monitoring.py
+++ monitoring-scripts/trunk/monitoring/wlcg-glue2-monitoring.py
@@ -10,14 +10,22 @@ from cStringIO import StringIO
 import sites
 import glue2_monitor
 import obsolete_entries
+import waiting_jobs
 
 path_to_command = "/afs/cern.ch/user/m/malandes/public/glue-validator/usr/bin"
 
 # Obsolete entries file output definition
-file_name = "/afs/cern.ch/user/m/malandes/www/web/ssb/wlcg/obsolete_entries/obsolete_entries.txt"
-ggus_file_name = "/afs/cern.ch/user/m/malandes/www/web/ssb/wlcg/obsolete_entries/ggus_obsolete_entries.txt"
-output = os.open (file_name, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0600)
-ggus_output = os.open (ggus_file_name, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0600)
+obs_file_name = "/afs/cern.ch/user/m/malandes/www/web/ssb/wlcg/obsolete_entries/obsolete_entries.txt"
+obs_ggus_file_name = "/afs/cern.ch/user/m/malandes/www/web/ssb/wlcg/obsolete_entries/ggus_obsolete_entries.txt"
+obs_output = os.open (obs_file_name, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0600)
+obs_ggus_output = os.open (obs_ggus_file_name, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0600)
+
+# Waiting jobs file output definition
+wait_file_name = "/afs/cern.ch/user/m/malandes/www/web/ssb/wlcg/waiting_jobs/waiting_jobs.txt"
+wait_ggus_file_name = "/afs/cern.ch/user/m/malandes/www/web/ssb/wlcg/waiting_jobs/ggus_waiting_jobs.txt"
+wait_output = os.open (wait_file_name, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0600)
+wait_ggus_output = os.open (wait_ggus_file_name, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0600)
+
 
 for site_name in sorted(sites.wlcg_site_bdiis.keys()):
 
@@ -32,9 +40,17 @@ for site_name in sorted(sites.wlcg_site_bdiis.keys()):
     print ("%s %s %s %s %s" % (dt,site_name,result,color,file_url))
 
     # Obsolete entries
-    obsolete_entries.obsolete_entries (site_name, sites.wlcg_site_bdiis[site_name], output, ggus_output, full_text) 
+    #obsolete_entries.obsolete_entries (site_name, sites.wlcg_site_bdiis[site_name], obs_output, obs_ggus_output, full_text) 
+
+    # Waiting jobs
+    waiting_jobs.waiting_jobs (site_name, sites.wlcg_site_bdiis[site_name], wait_output, wait_ggus_output, full_text)
 
 # Obsolete entries file output closing
-os.close(output)
-os.close(ggus_output)
+os.close(obs_output)
+os.close(obs_ggus_output)
+
+# Waiting jobs file output closing
+os.close(wait_output)
+os.close(wait_ggus_output)
+
 
